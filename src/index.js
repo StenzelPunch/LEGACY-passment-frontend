fddfd174a4048672ea70a97aa4234c4e5a80733d
Stenzel <stenzel.punch@gmail.com>
2020-01-18 18:35:23 +0200

    + prerender

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import App from './App';
 import './index.css';
 import 'normalize.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+    <App/>,
+    document.getElementById('root')
+  );

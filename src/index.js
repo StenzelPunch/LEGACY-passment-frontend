580f28761576cdc87738501bf99ff56b43bee6ae
Stenzel <stenzel.punch@gmail.com>
2020-02-13 10:14:24 +0200

    new prerender

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { render } from 'react-snapshot';
 import App from './App';
 import './i18n';
 import './index.css';
 import 'normalize.css';
 
-render(
-    <App/>,
-    document.getElementById('root')
-  );
+import { hydrate, render } from "react-dom";
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}

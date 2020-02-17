c068b0f802398b86804b50756cb65cd4d5375a91
Stenzel <stenzel.punch@gmail.com>
2020-02-17 16:46:25 +0200

    add link parser

--- src/Components/ContactsItem/ContactsItem.jsx
+++ src/Components/ContactsItem/ContactsItem.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./ContactsItem.css";
+import linkParser from "../../utils/linkParser"
 
 function ContactsItem(props) {
     return (
         <div className="contacts-item">
-            <a className="item-link" href={props.link}>
+            <a className="item-link" href={linkParser(props.name ,props.link)}>
                 <img className="item-image" src={"/images/" + [props.name] + ".svg"} alt={props.name + "-icon"} />
             </a>
         </div>
---
+++ src/utils/linkParser.js
@@ -0,0 +1,74 @@
+export default function linkParser(name, link) {
+    switch (name) {
+        case "phone":
+            if (link.match(/tel:\+?[0-9]*/g)) {
+                return link;
+            } else {
+                return "tel:" + link;
+            }
+        case "email":
+            if (link.match(/mailto:.*/g)) {
+                return link;
+            } else {
+                return "mailto:" + link;
+            }
+        case "website":
+            if (link.match(/https:\/\/.*/g)) {
+                return link;
+            } else {
+                return "https://" + link;
+            }
+        case "telegram":
+            if (link.match(/https:\/\/t\.me\/.*/g)) {
+                return link;
+            } else {
+                return "https://t.me/" + link;
+            }
+        case "viber":
+            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+                return "viber://add?number=" + link.replace("+", "%2B")
+               } else {
+                return "viber://add?number=" + link 
+               }
+        case "whatsapp":
+            if (link.match(/https:\/\/api\.whatsapp\.com\/send\?phone=.*/g)) {
+                return link;
+            } else {
+                if (link.match(/\+380[0-9]*/g)) {
+                    return "https://api.whatsapp.com/send?phone=" + link;
+                } else if (link.match(/380[0-9]*/g)) {
+                    return "https://api.whatsapp.com/send?phone=+" + link;
+                } else {
+                    return "https://api.whatsapp.com/send?phone=+38" + link;
+                }
+            }
+        case "messenger":
+            if (link.match(/https:\/\/me\.me\/.*/g)) {
+                return link;
+            } else {
+                return "https://me.me/" + link;
+            }
+        case "facebook":
+            return link;
+        case "linkedin":
+            return link;
+        case "instagram":
+            if (link.match(/https:\/\/www.instagram.com\/.*/g)) {
+                return link;
+            } else {
+                return "https://www.instagram.com/" + link;
+            }
+        case "twitter":
+            return link;
+        case "youtube":
+            return link;
+        case "skype":
+            return link;
+        case "vimeo":
+            return link;
+        case "gmaps":
+            return link;
+        default:
+            return link;
+    }
+}

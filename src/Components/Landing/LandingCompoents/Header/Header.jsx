021154fdd99cc2b2c33294d7cfeaba4a20aa1d65
Stenzel <stenzel.punch@gmail.com>
2020-02-17 17:17:41 +0200

    Languages select

--- src/Components/Landing/LandingCompoents/Header/Header.jsx
+++ src/Components/Landing/LandingCompoents/Header/Header.jsx
@@ -11,13 +11,13 @@ function Header({ i18n }) {
                         <img className="header-logo__img" src="/images/logo.svg" alt="PassMent logotype" />
                     </div>
                     <div className="header-languages">
-                        <div className="header-languages__item" onClick={() => i18n.changeLanguage("ua")}>
+                        <div className={`header-languages__item ${i18n.language === "ua" ? " header-languages__selected" : ""}`} onClick={() => i18n.changeLanguage("ua")}>
                             UA
                         </div>
-                        <div className="header-languages__item" onClick={() => i18n.changeLanguage("ru")}>
+                        <div className={`header-languages__item ${i18n.language === "ru" ? " header-languages__selected" : ""}`} onClick={() => i18n.changeLanguage("ru")}>
                             RU
                         </div>
-                        <div className="header-languages__item" onClick={() => i18n.changeLanguage("en")}>
+                        <div className={`header-languages__item ${i18n.language === "en" ? " header-languages__selected" : ""}`} onClick={() => i18n.changeLanguage("en")}>
                             EN
                         </div>
                     </div>

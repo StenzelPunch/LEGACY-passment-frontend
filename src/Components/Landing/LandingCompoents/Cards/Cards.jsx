f5b0ef3a465aa260602ee4cbc6daba6acb8e9819
Stenzel <stenzel.punch@gmail.com>
2020-07-31 17:11:02 +0300

    bump

--- src/Components/Landing/LandingCompoents/Cards/Cards.jsx
+++ src/Components/Landing/LandingCompoents/Cards/Cards.jsx
@@ -3,45 +3,43 @@ import { withNamespaces } from "react-i18next";
 
 import "./Cards.scss";
 
-const prices = [100, 50, 100, 120];
+const prices = [100, 50, 100, 60];
 
-const Card = withNamespaces()(function({ t, index, onOrder, link}) {
-    return (
-        <div className="card-container">
-            <div className="card">
-                <div className="card-image">
-                    <img
-                        className="card-image__img"
-                        src={`/images/card-image-${index + 1}.png`}
-                        alt="Продукт PassMent"
-                    />
-                </div>
-                <h3 className="card-title">{t("card-title", { returnObjects: true })[index]}</h3>
-                <p className="card-text">{t("card-text", { returnObjects: true })[index]}</p>
-                <p className="card-price">
-                    {t("card-price", { returnObjects: true })[index][0]}
-                    <span className="card-price__bold">{` ${prices[index]} `}</span>
-                    {t("card-price", { returnObjects: true })[index][1]}
-                </p>
-                <button className="btn" onClick={onOrder}>{t("card-btn")}</button>
-            </div>
+const Card = withNamespaces()(function ({ t, index, onOrder, link }) {
+  return (
+    <div className="card-container">
+      <div className="card">
+        <div className="card-image">
+          <img className="card-image__img" src={`/images/card-image-${index + 1}.png`} alt="Продукт PassMent" />
         </div>
-    );
+        <h3 className="card-title">{t("card-title", { returnObjects: true })[index]}</h3>
+        <p className="card-text">{t("card-text", { returnObjects: true })[index]}</p>
+        <p className="card-price">
+          {t("card-price", { returnObjects: true })[index][0]}
+          <span className="card-price__bold">{` ${prices[index]} `}</span>
+          {t("card-price", { returnObjects: true })[index][1]}
+        </p>
+        <button className="btn" onClick={onOrder}>
+          {t("card-btn")}
+        </button>
+      </div>
+    </div>
+  );
 });
 
 function Cards({ t, onOrder }) {
-    return (
-        <div className="cards">
-            <div className="container">
-                <nav className="wrapper">
-                    <Card index={0} onOrder={onOrder}/>
-                    <Card index={1} onOrder={onOrder}/>
-                    <Card index={2} onOrder={onOrder}/>
-                    <Card index={3} onOrder={onOrder}/>
-                </nav>
-            </div>
-        </div>
-    );
+  return (
+    <div className="cards">
+      <div className="container">
+        <nav className="wrapper">
+          <Card index={0} onOrder={onOrder} />
+          <Card index={1} onOrder={onOrder} />
+          <Card index={2} onOrder={onOrder} />
+          <Card index={3} onOrder={onOrder} />
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default withNamespaces()(Cards);
--- src/Components/Landing/LandingCompoents/Header/Header.jsx
+++ src/Components/Landing/LandingCompoents/Header/Header.jsx
@@ -3,28 +3,34 @@ import { withNamespaces } from "react-i18next";
 import "./Header.scss";
 
 function Header({ i18n }) {
-    return (
-        <header>
-            <div className="container">
-                <div className="header">
-                    <div className="header-logo">
-                        <img className="header-logo__img" src="/images/logo.svg" alt="PassMent logotype" />
-                    </div>
-                    <div className="header-languages">
-                        <div className={`header-languages__item ${i18n.language === "ua" ? " header-languages__selected" : ""}`} onClick={() => i18n.changeLanguage("ua")}>
-                            UA
-                        </div>
-                        <div className={`header-languages__item ${i18n.language === "ru" ? " header-languages__selected" : ""}`} onClick={() => i18n.changeLanguage("ru")}>
-                            RU
-                        </div>
-                        <div className={`header-languages__item ${i18n.language === "en" ? " header-languages__selected" : ""}`} onClick={() => i18n.changeLanguage("en")}>
-                            EN
-                        </div>
-                    </div>
-                </div>
+  return (
+    <header>
+      <div className="container">
+        <div className="header">
+          <div className="header-logo">
+            <img className="header-logo__img" src="/images/logo.svg" alt="PassMent logotype" />
+          </div>
+          <div className="header-languages">
+            <div
+              className={`header-languages__item ${i18n.language === "ua" ? " header-languages__selected" : ""}`}
+              onClick={() => i18n.changeLanguage("ua")}
+            >
+              UA
             </div>
-        </header>
-    );
+            <div
+              className={`header-languages__item ${i18n.language === "ru" ? " header-languages__selected" : ""}`}
+              onClick={() => i18n.changeLanguage("ru")}
+            >
+              RU
+            </div>
+            {/* <div className={`header-languages__item ${i18n.language === "en" ? " header-languages__selected" : ""}`} onClick={() => i18n.changeLanguage("en")}>
+                            EN
+                        </div> */}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default withNamespaces()(Header);
